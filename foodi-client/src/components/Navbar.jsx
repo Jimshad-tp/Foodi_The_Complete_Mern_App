8827a107240bdf5b42e6bdf98481793b074e4a4f
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-12-16 16:31:31 +0530

    home page updated

--- foodi-client/src/components/Navbar.jsx
+++ foodi-client/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "/logo.png";
 import { FiPhoneCall } from "react-icons/fi";
 
 function Navbar() {
+  const [isSticky, setSticky] = useState(false)
+
+  // handld scroll function
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setSticky(true)
+      } else {
+        setSticky(false)
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.addEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   const Navitem = (
     <>
       <li>
@@ -46,8 +66,8 @@ function Navbar() {
     </>
   );
   return (
-    <header className="max-w-screen-2x1 container mx-auto">
-      <div className="navbar xl:px-24">
+    <header className="max-w-screen-2x1 container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out ">
+      <div className={`navbar xl:px-24 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" : ""}`}>
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -83,10 +103,10 @@ function Navbar() {
 
         <div className="navbar-end">
           {/*search button */}
-        <button className="btn btn-ghost btn-circle  hidden lg:flex">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
-    {/*Cart button*/}
+          <button className="btn btn-ghost btn-circle  hidden lg:flex">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </button>
+          {/*Cart button*/}
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle lg:flex items-center justify-center hidden">
             <div className="indicator">
               <svg
@@ -96,7 +116,7 @@ function Navbar() {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                
+
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
