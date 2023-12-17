d76c1ee5d00cfa0200147c0ffdef61ac6726c213
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-12-17 07:29:17 +0530

    special dishes added

--- foodi-client/src/pages/home/Home.jsx
+++ foodi-client/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import Category from './Category'
+import SpecialDishes from './SpecialDishes'
 
 function Home() {
   return (
     <div>
     <Banner/>
     <Category/>
+    <SpecialDishes/>
     </div>
   )
 }
